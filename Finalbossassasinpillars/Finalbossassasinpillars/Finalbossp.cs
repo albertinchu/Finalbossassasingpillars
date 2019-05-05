2337f52efe41c7b997051a958efc9b57f7c97cbd
albertinchu Martin Palomino <albertinchu@hotmail.com>
2019-05-05 17:09:10 +0200

    Finalizado

--- Finalbossassasinpillars/Finalbossassasinpillars/Finalbossp.cs
+++ Finalbossassasinpillars/Finalbossassasinpillars/Finalbossp.cs
@@ -3,42 +3,42 @@ using Smod2.Attributes;
 using scp4aiur;
 namespace Finalbossassasinpillars
 {
-    [PluginDetails(
-        author = "Albertinchu",
-        name = "Finalbossassasinpillars",
-        description = "4 SCPS 173 con 18000 de vida aproximadamente, muchos jugadores...",
-        id = "albertinchu.gamemode.Finalbossassasinpillars",
-        version = "1.0.0",
-        SmodMajor = 3,
-        SmodMinor = 0,
-        SmodRevision = 0
-        )]
-    public class Finalbossassasinpillars : Plugin
-    {
+	[PluginDetails(
+		author = "Albertinchu",
+		name = "Finalbossassasinpillars",
+		description = "4 SCPS 173 con 18000 de vida aproximadamente, muchos jugadores...",
+		id = "albertinchu.gamemode.Finalbossassasinpillars",
+		version = "1.0.0",
+		SmodMajor = 3,
+		SmodMinor = 0,
+		SmodRevision = 0
+		)]
+	public class Finalbossassasinpillars : Plugin
+	{
 
-        public override void OnDisable()
-        {
-            this.Info("Finalbossassasinpillars - Desactivado");
-        }
+		public override void OnDisable()
+		{
+			this.Info("Finalbossassasinpillars - Desactivado");
+		}
 
-        public override void OnEnable()
-        {
-            Info("Finalbossassasinpillars - activado.");
-        }
+		public override void OnEnable()
+		{
+			Info("Finalbossassasinpillars - activado.");
+		}
 
-        public override void Register()
-        {
-            GamemodeManager.Manager.RegisterMode(this);
-            Timing.Init(this);
-            this.AddEventHandlers(new PlayersEvents(this));
+		public override void Register()
+		{
+			GamemodeManager.Manager.RegisterMode(this);
+			Timing.Init(this);
+			this.AddEventHandlers(new PlayersEvents(this));
 
-        }
-        public void RefreshConfig()
-        {
+		}
+		public void RefreshConfig()
+		{
 
 
-        }
-    }
+		}
+	}
 
 }
 
